06aa08e2d014ae2dd21070cc5a4931c6868de62b
Dimitar Petrov <d.petrov-ab@student.fontys.nl>
2022-05-07 15:27:00 +0200

    adding auth

--- src/main/java/com/ludis/gateway/GatewayApplication.java
+++ src/main/java/com/ludis/gateway/GatewayApplication.java
@@ -1,15 +1,30 @@
 package com.ludis.gateway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
+
+@RestController
 @SpringBootApplication
 @EnableEurekaClient
 public class GatewayApplication {
+
+	@RequestMapping("/fallback")
+	public Mono<String> fallback() {
+		return Mono.just("fallback");
+	}
 
 	@Bean
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
@@ -21,16 +36,19 @@ public class GatewayApplication {
 				.route("rewrite_route", r -> r.host("*.rewrite.org")
 						.filters(f -> f.rewritePath("/foo/(?<segment>.*)", "/${segment}"))
 						.uri("http://httpbin.org"))
-//				.route(p -> p
-//						.host("*.circuitbreaker.com")
-//						.filters(f -> f.circuitBreaker(config -> config
-//								.setName("mycmd")
-//								.setFallbackUri("forward:/fallback")))
-//						.uri("http://httpbin.org:9000"))
+				.route(p -> p
+						.host("*.circuitbreaker.com")
+						.filters(f -> f.circuitBreaker(config -> config
+								.setName("mycircuit")
+								.setFallbackUri("forward:/fallback")))
+						.uri("http://httpbin.org:9000"))
 				.build();
 	}
 
+
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
