2c3dd1868f5127d4771970600fcc69b922cf6b9a
Dimitar Petrov <d.petrov-ab@student.fontys.nl>
2022-06-07 19:46:23 +0200

    fix

--- src/main/java/com/ludis/gateway/GatewayApplication.java
+++ src/main/java/com/ludis/gateway/GatewayApplication.java
@@ -1,22 +1,15 @@
 package com.ludis.gateway;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 
-@RestController
 @SpringBootApplication
 @EnableEurekaClient
 public class GatewayApplication {
