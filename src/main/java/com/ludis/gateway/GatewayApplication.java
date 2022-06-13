2481b3cf4c29a80f0bff8258a216d9ad86bac936
Dimitar Petrov <d.petrov-ab@student.fontys.nl>
2022-06-13 17:56:20 +0200

    Update GatewayApplication.java

--- src/main/java/com/ludis/gateway/GatewayApplication.java
+++ src/main/java/com/ludis/gateway/GatewayApplication.java
@@ -19,28 +19,28 @@ public class GatewayApplication {
 		return Mono.just("fallback");
 	}
 
-	@Bean
-	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route("path_route", r -> r.path("/users/all")
-						.uri("http://user-server:9000"))
-				.route("path_route", r -> r.path("/auth/hello")
-						.uri("http://auth-server:8004"))
-				.route("path_route", r -> r.path("/calories/all")
-						.uri("http://calories-server:9200"))
-				.route("path_route", r -> r.path("/steps/all")
-						.uri("http://steps-server:9100"))
-				.route("rewrite_route", r -> r.host("*.rewrite.org")
-						.filters(f -> f.rewritePath("/foo/(?<segment>.*)", "/${segment}"))
-						.uri("http://httpbin.org"))
-				.route(p -> p
-						.host("*.circuitbreaker.com")
-						.filters(f -> f.circuitBreaker(config -> config
-								.setName("mycircuit")
-								.setFallbackUri("forward:/fallback")))
-						.uri("http://httpbin.org:9000"))
-				.build();
-	}
+//	@Bean
+//	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//		return builder.routes()
+//				.route("path_route", r -> r.path("/users/all")
+//						.uri("http://user-server:9000"))
+//				.route("path_route", r -> r.path("/auth/hello")
+//						.uri("http://auth-server:8004"))
+//				.route("path_route", r -> r.path("/calories/all")
+//						.uri("http://calories-server:9200"))
+//				.route("path_route", r -> r.path("/steps/all")
+//						.uri("http://steps-server:9100"))
+//				.route("rewrite_route", r -> r.host("*.rewrite.org")
+//						.filters(f -> f.rewritePath("/foo/(?<segment>.*)", "/${segment}"))
+//						.uri("http://httpbin.org"))
+//				.route(p -> p
+//						.host("*.circuitbreaker.com")
+//						.filters(f -> f.circuitBreaker(config -> config
+//								.setName("mycircuit")
+//								.setFallbackUri("forward:/fallback")))
+//						.uri("http://httpbin.org:9000"))
+//				.build();
+//	}
 
 	public static void main(String[] args) {
 
